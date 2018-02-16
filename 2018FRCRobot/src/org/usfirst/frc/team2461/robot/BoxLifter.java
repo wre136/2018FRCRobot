3e75928384fb4fbcfd950b9f0245a6effa88b8c3
William_Edds <William_Edds@W10DZZYR32.amer.dell.com>
2018-02-16 12:13:51 -0600

    Added comments and changed motor control calls in BoxLifter class

--- 2018FRCRobot/src/org/usfirst/frc/team2461/robot/BoxLifter.java
+++ 2018FRCRobot/src/org/usfirst/frc/team2461/robot/BoxLifter.java
@@ -4,6 +4,13 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.SpeedController;
 
+/**
+ * <h1> Box Lifter Class </h1>
+ * @author William R Edds FRC 2461 - The METAL-SKINs
+ * <p>
+ * Box Lifter subsystem that is used to raise and lower the box Collector
+ * </p>
+ */
 public class BoxLifter
 {
 	private SpeedController motorLifter;
@@ -25,6 +32,8 @@ public class BoxLifter
 	private Mode mode;
 	
 	/**
+	 * Creates a BoxLifter object that has a middle switch.
+	 * This will also set the lifter to be used in AUTOMATIC mode.
 	 * @param motorIn Lift motor
 	 * @param switchLowerIn Switch for lower point
 	 * @param switchMiddleIn Switch for mid point
@@ -42,6 +51,14 @@ public class BoxLifter
 		mode = Mode.AUTOMATIC;
 	}
 	
+	/**
+	 * Creates a BoxLifter object that does not have a middle switch.
+	 * This will also set the lifter to be used in MANUAL mode.
+	 * @param motorIn Lift motor
+	 * @param switchLowerIn Switch for lower point
+	 * @param switchHigherIn switch for high point
+	 * @param playerIn MetalSkinsController to control BoxLifter
+	 */
 	public BoxLifter(SpeedController motorIn, DigitalInput switchLowerIn, DigitalInput switchHigherIn, MetalSkinsController playerIn) {
 		motorLifter = motorIn;
 		motorLifter.setInverted(true);
@@ -52,6 +69,14 @@ public class BoxLifter
 		mode = Mode.MANUAL;
 	}
 	
+	/**
+	 * This is the method that runs the box lifter in teleop.
+	 * 
+	 * <p>
+	 * Call this method in teleop to run the box lifter using
+	 * the State Machine mechanics and controller input.
+	 * </p>
+	 */
 	public void run() {
 		switch(stateNow) {
 			case BEGIN:
@@ -81,81 +106,112 @@ public class BoxLifter
 		}
 	}
 	
+	/**
+	 * Method for the BEGIN State of the box lifter state machine.
+	 * It stops the lifter motor and then sets the state to LOW, 
+	 * MIDDLE, HIGH or IDLE depending on whether any of the switches 
+	 * are currently flipped
+	 */
 	private void begin() {
-		motorLifter.set(0);
-		stateNow = State.IDLE;
-	}
-	
-	private void idle() {
-		if(mode == Mode.AUTOMATIC) {
-			if(!switchLower.get()) {
-				stateNow = State.LOW;
-			} else if(!switchMiddle.get()) {
-				stateNow = State.MIDDLE;
-			} else if(!switchHigher.get()) {
-				stateNow = State.HIGH;
-			} else if(player.getBumper(Hand.kRight)) {
-				motorLifter.set(1);
-				stateNow = State.LIFTING;
-			} else if(player.getBumper(Hand.kLeft)) {
-				motorLifter.set(-1);
-				stateNow = State.LOWERING;
-			}
+		stop();
+			
+		if(getSwitchLow()) {
+			stateNow = State.LOW;
+		} else if(getSwitchHigh()) {
+			stateNow = State.HIGH;
+		} else if(getSwitchMiddle()) {
+			stateNow = State.MIDDLE;
 		} else {
-			if(!switchLower.get()) {
-				stateNow = State.LOW;
-			} else if(!switchHigher.get()) {
-				stateNow = State.HIGH;
-			} else if(player.getBumper(Hand.kRight)) {
-				motorLifter.set(1);
-				stateNow = State.LIFTING;
-			} else if(player.getBumper(Hand.kLeft)) {
-				motorLifter.set(-1);
-				stateNow = State.LOWERING;
-			}
+			stateNow = State.IDLE;
 		}
-		
 	}
 	
+	/**
+	 * Method for the IDLE State of the box lifter state machine.
+	 * This is the state where the lifter will not be moving.
+	 * Based on controller inputs, the right bumper will lift the box
+	 * Collector and set the state to LIFTING. The left bumper will 
+	 * lower the box lifter and set the state to LOWERING.
+	 */
+	private void idle() {
+		if(player.getBumper(Hand.kRight)) {
+			rise();
+			stateNow = State.LIFTING;
+		} else if(player.getBumper(Hand.kLeft)) {
+			lower();
+			stateNow = State.LOWERING;
+		}
+	}
+	
+	/**
+	 * Method for the LOWERING state of the box lifter state machine.
+	 * This is the state where the lifter will be lowering.
+	 * 
+	 * <p>
+	 * If the lifter is in automatic mode, it will lower until it hits the middle 
+	 * switch where it will stop the motor and go to MIDDLE state, or until it 
+	 * hits the low switch where it will stop the motor and go to LOW state.
+	 * </p>
+	 * <p>
+	 * If the lifter is in manual mode, it will lower until either the low switch
+	 * is hit and goes to LOW state or until the player lets go of the left bumper on
+	 * the controller and goes to IDLE state.
+	 * </p>
+	 */
 	private void lowering() {
 		if(mode == Mode.AUTOMATIC) { // If mode is Automatic
 			if(!switchMiddle.get()) {
-				motorLifter.set(0);
+				stop();
 				stateNow = State.MIDDLE;
 			} else if(!switchLower.get()) {
-				motorLifter.set(0);
+				stop();
 				stateNow = State.LOW;
 			}
 		} else { // If mode is Manual
 			if(switchLower.get()) { // If not at the bottom
 				if(!player.getBumper(Hand.kLeft)) { //If player lets go of the down button
-					motorLifter.set(0); //Turn motor off
+					stop(); //Turn motor off
 					stateNow = State.IDLE; // Set State to Idle
 				}
 			} else { // Turn motor off and set State to Low
-				motorLifter.set(0);
+				stop();
 				stateNow = State.LOW;
 			}
 		}
 	}
 	
+	/**
+	 * Method for the LIFTING state of the box lifter state machine.
+	 * This is the state where the lifter will be lifting.
+	 * 
+	 * <p>
+	 * If the lifter is in automatic mode, it will lift until it hits the middle 
+	 * switch where it will stop the motor and go to MIDDLE state, or until it 
+	 * hits the high switch where it will stop the motor and go to HIGH state.
+	 * </p>
+	 * <p>
+	 * If the lifter is in manual mode, it will lift until either the high switch
+	 * is hit and goes to HIGH state or until the player lets go of the right bumper on
+	 * the controller and goes to IDLE state.
+	 * </p>
+	 */
 	private void lifting() {
 		if(mode == Mode.AUTOMATIC) { // if mode is Automatic
 			if(!switchMiddle.get()) {
-				motorLifter.set(0);
+				stop();
 				stateNow = State.MIDDLE;
 			} else if(switchHigher.get()) {
-				motorLifter.set(0);
+				stop();
 				stateNow = State.HIGH;
 			}
 		} else { // if mode is Manual
 			if(switchHigher.get()) { //if not at the Top
 				if(!player.getBumper(Hand.kRight)) { // If player lets go of the up button
-					motorLifter.set(0); //Turn motor off
+					stop(); //Turn motor off
 					stateNow = State.IDLE; // Set State to Idle
 				}
 			} else { // Turn off motor and set State to High
-				motorLifter.set(0);
+				stop();
 				stateNow = State.HIGH;
 			}
 		}
@@ -163,26 +219,52 @@ public class BoxLifter
 		
 	}
 	
+	/**
+	 * Method for the LOW state of the box lifter state machine.
+	 * <p>
+	 * This is the state where the lifter will be stopped in the
+	 * low position. When the right bumper on the controller is pressed,
+	 * The lifter will lift and go to the LIFTING state.
+	 * </p>
+	 */
 	private void low() {
 		if(player.getBumper(Hand.kRight)) {
-			motorLifter.set(1);
+			rise();
 			stateNow = State.LIFTING;
 		}
 	}
 	
+	/**
+	 * Method for the MIDDLE state of the box lifter state machine.
+	 * <p>
+	 * This is the state where the lifter will be stopped in the
+	 * middle position. If the right bumper on the controller is pressed,
+	 * The lifter will lift and go to the LIFTING state. If the left 
+	 * bumper on the controller is pressed, the lifter will lower and
+	 * go to the LOWERING state.
+	 * </p>
+	 */
 	private void middle() {
 		if(player.getBumper(Hand.kRight)) {
-			motorLifter.set(1);
+			rise();
 			stateNow = State.LIFTING;
 		} else if(player.getBumper(Hand.kLeft)) {
-			motorLifter.set(-1);
+			lower();
 			stateNow = State.LOWERING;
 		}
 	}
 	
+	/**
+	 * Method for the HIGH state of the box lifter state machine.
+	 * <p>
+	 * This is the state where the lifter will be stopped in the
+	 * high position. When the left bumper on the controller is pressed,
+	 * The lifter will lower and go to the LOWERING state.
+	 * </p>
+	 */
 	private void high() {
 		if(player.getBumper(Hand.kLeft)) {
-			motorLifter.set(-1);
+			lower();
 			stateNow = State.LOWERING;
 		}
 	}
@@ -191,6 +273,10 @@ public class BoxLifter
 		return stateNow;
 	}
 	
+	/**
+	 * Returns the current state of the Box Lifter State Machine as a String object
+	 * @return Current state of the Box Lifter state machine as a String object
+	 */
 	public String getStateString() {
 		switch(stateNow) {
 			case BEGIN:
@@ -212,26 +298,47 @@ public class BoxLifter
 		}
 	}
 	
+	/**
+	 * Lowers the box lifter by setting the motor to -1 
+	 */
 	public void lower() {
 		motorLifter.set(-1);
 	}
 	
+	/**
+	 * Lifts the box lifter by setting the motor to 1
+	 */
 	public void rise() {
 		motorLifter.set(1);
 	}
 	
+	/**
+	 * Stops the box lifter by setting the motor to 0
+	 */
 	public void stop() {
-		
+		motorLifter.set(0);;
 	}
 	
+	/**
+	 * Get the current status of the low position switch
+	 * @return True if the switch is flipped, false otherwise.
+	 */
 	public boolean getSwitchLow() {
 		return switchLower.get();
 	}
 	
+	/**
+	 * Get the current status of the high position switch
+	 * @return True if the switch is flipped, false otherwise.
+	 */
 	public boolean getSwitchHigh() {
 		return switchHigher.get();
 	}
 	
+	/**
+	 * Get the current status of the middle position switch
+	 * @return True if the switch is flipped, false otherwise.
+	 */
 	public boolean getSwitchMiddle() {
 		return switchMiddle.get();
 	}
