e243d4aac16ce3a60261165e319bad2d06aeac54
wre136@hotmail.com <wre136@hotmail.com>
2018-03-04 15:56:35 -0600

    Updated package-info.java for autonomous package

--- 2018FRCRobot/src/org/usfirst/frc/team2461/robot/autonomous/package-info.java
+++ 2018FRCRobot/src/org/usfirst/frc/team2461/robot/autonomous/package-info.java
@@ -1,8 +1,8 @@
 /**
- * 
+ * Domain classes that control the robot during autonomous periodic
  */
 /**
- * @author wre13
+ * @author William R Edds FRC 2461 - The METAL-SKINs
  *
  */
 package org.usfirst.frc.team2461.robot.autonomous;
